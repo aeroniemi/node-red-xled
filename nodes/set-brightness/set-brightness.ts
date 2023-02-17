f08527a2f60188b34e21320af07e2d679c1f8edd
Alexander Beavil <alex@aeroniemi.com>
2023-02-17 17:49:48 +0000

    Tidied up set-brightness

--- nodes/set-brightness/set-brightness.ts
+++ nodes/set-brightness/set-brightness.ts
@@ -13,49 +13,47 @@ interface setBrightnessConfig {
 	brightness: number;
 	override: boolean;
 }
-export interface setBrightnessNode extends Node, setBrightnessConfig {
-	server: xledServerNode;
-}
 export interface setBrightnessNodeDef extends NodeDef, setBrightnessConfig {}
 
 // create node
-export default function (RED: any): void {
-	function setBrightnessNode(
-		this: setBrightnessNode,
-		config: setBrightnessNodeDef
-	): void {
-		RED.nodes.createNode(this, config);
-		// check server exists
-		try {
-			this.server = RED.nodes.getNode(config.server);
-		} catch (err) {
-			this.error("XLED Server not found");
-			return;
-		}
-		// set config
-		this.brightness = config.brightness;
-		this.override = config.override;
-		const node = this; // makes typescript happy when using it inside node.on()
-		node.on("input", async function (msg: any, send, done) {
-			await setBrightness(node, msg, done);
-		});
-	}
-	RED.nodes.registerType("set-brightness", setBrightnessNode);
-}
-async function setBrightness(node: setBrightnessNode, msg: any, done: any) {
-	try {
-		let brightness: number = node.override
-			? node.brightness
-			: msg.brightness || node.brightness;
 
-		if (brightness > 100 || brightness < 0)
-			throw new RangeError("Brightness must be between 0 and 100 inclusive");
-		node.debug("Calling login for brightness");
-		await node.server.light.ensureLoggedIn();
-		await node.server.light.setBrightness(brightness);
-		node.debug(`Set brightness to ${brightness}`);
-	} catch (err: any) {
-		done(err);
+export default function (RED: any): void {
+	RED.nodes.registerType("set-brightness", brightness);
+	function brightness(this: Node, config: setBrightnessNodeDef) {
+		return setBrightnessNode(this, config, RED);
 	}
-	done();
+}
+
+function setBrightnessNode(
+	node: any,
+	config: setBrightnessNodeDef,
+	RED: any
+): void {
+	// Create the node
+	RED.nodes.createNode(node, config);
+	// Check server exists
+	try {
+		node.server = RED.nodes.getNode(config.server);
+	} catch (err) {
+		node.error("XLED Server not found");
+		return;
+	}
+	node.on("input", async function (msg: any, send: any, done: any) {
+		try {
+			await node.server.light.ensureLoggedIn();
+			let brightness: number = Number(
+				config.override
+					? config.brightness
+					: msg.brightness || config.brightness
+			);
+			if (brightness > 100 || brightness < 0)
+				throw new RangeError("Brightness must be between 0 and 100 inclusive");
+
+			await node.server.light.setBrightness(brightness);
+			node.debug(`Set brightness to ${brightness}`);
+		} catch (err: any) {
+			done(err);
+		}
+		done();
+	});
 }
